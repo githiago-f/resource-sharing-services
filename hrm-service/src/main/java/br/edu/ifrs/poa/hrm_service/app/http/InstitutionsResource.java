1a105128a7b3aafc118c1bc2d1ee2ba7e9a483a0
githiago-f <tfarias@protonmail.com>
2023-04-02 22:49:36 -0300

    feat: add lombok and map javax to jakarta

--- hrm-service/src/main/java/br/edu/ifrs/poa/hrm_service/app/http/InstitutionsResource.java
+++ hrm-service/src/main/java/br/edu/ifrs/poa/hrm_service/app/http/InstitutionsResource.java
@@ -4,10 +4,10 @@ import br.edu.ifrs.poa.hrm_service.core.institutional.Institution;
 import br.edu.ifrs.poa.hrm_service.core.institutional.Resource;
 import br.edu.ifrs.poa.hrm_service.core.institutional.vo.ResourceStatus;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.Set;
 import java.util.UUID;
